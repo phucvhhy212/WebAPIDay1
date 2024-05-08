c67d7f3486395ab03a8d35220b677a23eb8855de
phucvhhy212 <phucvhhy212@gmail.com>
2024-05-08 16:13:53 +0700

    try-catch / logger

--- BusinessLogic/TaskService.cs
+++ BusinessLogic/TaskService.cs
@@ -1,9 +1,15 @@
-﻿using Task = DataAccess.Task;
+﻿using Microsoft.Extensions.Logging;
+using Task = DataAccess.Task;
 
 namespace BusinessLogic
 {
     public class TaskService : ITaskService
     {
+        private readonly ILogger<TaskService> _logger;
+        public TaskService(ILogger<TaskService> logger)
+        {
+            _logger = logger;
+        }
         private static List<Task> TaskData()
         {
             return new List<Task>
@@ -19,47 +25,61 @@ namespace BusinessLogic
         private static List<Task> data = TaskData();
         public IEnumerable<Task> GetAll()
         {
+            _logger.Log(LogLevel.Information,"Getting all tasks");
             return data;
         }
 
         public Task? GetById(Guid id)
         {
+            _logger.Log(LogLevel.Information, $"Getting task with id = {id}");
             return data.FirstOrDefault(x => x.Id == id);
         }
 
         public void Delete(Guid id)
         {
+            _logger.Log(LogLevel.Information, $"Finding task with id = {id} to delete");
             var taskToDelete = GetById(id);
             if (taskToDelete != null)
             {
+                _logger.Log(LogLevel.Information, $"Deleting task with id = {id}");
                 data.Remove(taskToDelete);
+            }
+            else
+            {
+                _logger.Log(LogLevel.Information, $"Task {id} not exist to delete");
+                throw new Exception($"Task {id} not exist");
             }
         }
 
         public void Edit(Guid id, Task task)
         {
+            _logger.Log(LogLevel.Information, $"Finding task with id = {id} to edit");
             var taskToUpdate = GetById(id);
             if (taskToUpdate != null)
             {
+                _logger.Log(LogLevel.Information, $"Updating task with id = {id}");
                 taskToUpdate.IsCompleted = task.IsCompleted;
                 taskToUpdate.Title = task.Title;
+            }
+            else
+            {
+                _logger.Log(LogLevel.Information, $"Task {id} not exist to edit");
+                throw new Exception($"Task {id} not exist");
             }
 
         }
 
         public void Add(Task task)
         {
-            var findTask = GetById(task.Id);
-            if (findTask == null)
-            {
-                data.Add(task);
-            }
+            _logger.Log(LogLevel.Information, $"Adding task {task.Id}");
+            data.Add(task);
         }
 
         public void AddRange(IEnumerable<Task> task)
         {
             if (!data.Any(x => task.Select(t => t.Id).Contains(x.Id)))
             {
+                _logger.Log(LogLevel.Information, $"Bulk add tasks");
                 data.AddRange(task);
             }
         }
@@ -72,12 +92,15 @@ namespace BusinessLogic
                 if (GetById(id) == null)
                 {
                     isValid = false;
-                    break;
+                    _logger.Log(LogLevel.Error, $"Task {id} exist");
+                    throw new Exception($"Task {id} not exist");
                 }
             }
 
             if (isValid)
             {
+                _logger.Log(LogLevel.Information, "All task exist to delete");
+                _logger.Log(LogLevel.Information, "Bulk delete task");
                 foreach (var id in taskIds)
                 {
                     Delete(id);
---
+++ WebApi/Controllers/TasksController.cs
@@ -0,0 +1,128 @@
+﻿using AutoMapper;
+using BusinessLogic;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using Task = DataAccess.Task;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly ITaskService _taskService;
+        private readonly IMapper mapper;
+
+
+        public TasksController(ITaskService taskService, IMapper mapper)
+        {
+            _taskService = taskService;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_taskService.GetAll());
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(TaskDto task)
+        {
+            try
+            {
+                _taskService.Add(mapper.Map<Task>(task));
+                return Ok("Created successfully");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            try
+            {
+                var task = _taskService.GetById(id);
+                if(task == null)
+                {
+                    return BadRequest("Task not found");
+                }
+                return Ok(task);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, TaskDto task)
+        {
+            try
+            {
+                _taskService.Edit(id, mapper.Map<Task>(task));
+                return Ok("Edit successfully");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                _taskService.Delete(id);
+                return Ok($"Delete task {id} successfully");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+        }
+
+        [HttpDelete("Bulk")]
+        public IActionResult Delete(IEnumerable<Guid> ids)
+        {
+            try
+            {
+                _taskService.DeleteRange(ids);
+                return Ok("Bulk delete successfully");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost("Bulk")]
+        public IActionResult Create(IEnumerable<TaskDto> tasks)
+        {
+            try
+            {
+                _taskService.AddRange(mapper.Map<IEnumerable<Task>>(tasks));
+                return Ok("Bulk create successfully");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
